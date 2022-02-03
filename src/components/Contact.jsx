72d371db8459b5aa9f48ba08581f5d382ed6cc78
Andrey <raigon28@gmail.com>
2022-02-04 00:23:58 +0300

    adaptive

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -13,15 +13,20 @@ const Contact = ({ name, phone, id }) => {
 
 	return (
 		<div className="contact">
-            <p className="contact__id">#{id + 1}</p>
+			<p className="contact__id">#{id + 1}</p>
 			<p className="contact__name">{name}</p>
 			<p className="contact__phone">+{phone}</p>
-			<Link className="contact__edit" to={`/edit/${id}`}>
-				Edit
-			</Link>
-			<button className="contact__delete" onClick={() => deliteCon(id)}>
-				Delete
-			</button>
+			<div className="contact__btns">
+				<Link className="contact__edit" to={`/edit/${id}`}>
+					Edit
+				</Link>
+				<button
+					className="contact__delete"
+					onClick={() => deliteCon(id)}
+				>
+					Delete
+				</button>
+			</div>
 		</div>
 	);
 };
